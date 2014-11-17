069e7b77ccc56261aef1c8cb8168072fa6aa5b4d
Dan Kaufman <daniel.garrett.kaufman@gmail.com>
2014-11-17 14:24:54 -0500

    Initial Commit

---
+++ BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Dan Kaufman on 11/17/14.
+//  Copyright (c) 2014 Dan Kaufman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ BNRQuizViewController.m
@@ -0,0 +1,20 @@
+//
+//  BNRQuizViewController.m
+//  Quiz
+//
+//  Created by Dan Kaufman on 11/17/14.
+//  Copyright (c) 2014 Dan Kaufman. All rights reserved.
+//
+
+#import "BNRQuizViewController.h"
+
+@interface BNRQuizViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
+@end
+
+@implementation BNRQuizViewController
+
+@end
